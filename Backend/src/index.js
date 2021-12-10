0afd8ce23fda84b2383a6e8d00cde3d38e6a9ff8
mvitta <mvitta@uninorte.edu.co>
2021-12-10 16:27:56 -0500

    desde el formulario de registro usuario de planta se envia una peticion hacia express

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,5 +1,5 @@
-const { log } = require("console");
 const exp = require("express");
+const cors = require("cors")
 const morgan = require("morgan");
 const path = require("path");
 const app = exp();
@@ -10,12 +10,13 @@ app.set("port", process.env.PORT || 4000);
 // Middlewares
 app.use(morgan("dev"));
 app.use(exp.json());
+app.use(cors())
 
 // routes
 app.use("/siAuto", require("./routes/routes"));
 
 // static files
-// app.use('/siAuto', exp.static(path.join(__dirname, 'public')))
+app.use('/siAuto', exp.static(path.join(__dirname, 'public')))
 
 app.listen(app.get("port"), () => {
   console.log(`Escuchando en http://localhost:${app.get("port")}/`);
--- Backend/src/routes/routes.js
+++ Backend/src/routes/routes.js
@@ -1,18 +1,27 @@
-const exp = require('express')
-const router = exp.Router()
+const exp = require("express");
+const router = exp.Router();
 
 router.get("/inicio", (req, res) => {
-    // send: para enviar texto
-    res.send("Hola Mundo desde el archivo reoutes !");
-  });
-  
-  router.get("/seccion1", (req, res) => {
-    // json: especifica que devulve un objeto de javascript
-    res.json({
-      nombre: "Jean",
-      apellido: "vitta",
-    });
-  });
-  
+  // send: para enviar texto
+  res.send("Hola Mundo desde el archivo reoutes !");
+});
 
-  module.exports = router;
+router.get("/seccion1", (req, res) => {
+  // json: especifica que devulve un objeto de javascript
+  res.json({
+    nombre: "Jean",
+    apellido: "vitta",
+  });
+});
+
+router.post("/salida", (req, res) => {
+  res.send("Peticion recibida");
+  // esto es lo que va en la base de datos
+  const datoDelFront = req.body;
+  datoDelFront.map((elemento) => {
+    console.log(elemento);
+  });
+ 
+});
+
+module.exports = router;
