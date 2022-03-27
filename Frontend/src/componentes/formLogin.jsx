aa3f83eb987b114a91a9512e6fcf039beccd5cae
mvitta <mvitta@uninorte.edu.co>
2022-03-27 13:20:10 -0500

    update 1

--- Frontend/src/componentes/formLogin.jsx
+++ Frontend/src/componentes/formLogin.jsx
@@ -1,132 +1,102 @@
 import React from "react";
 import { useNavigate } from "react-router-dom"; // V6 -> nuevos cambios
 import { UserContext } from "../context/UserContext";
-// import { getUsuario } from "../api/apiusuarios";
-// import { getRoles } from "../api/apiroles";
+import { fetchData } from "../services/request";
+import "../css/formLogin.css";
 
 export function Login() {
-  // Simulacion
-  // const [rolesusuarios, setRolesUsuarios] = useState([]);
   const { user, setUser } = React.useContext(UserContext);
-
   const navigate = useNavigate();
 
+  React.useEffect(
+    function () {
+      document
+        .getElementById("formulario")
+        .addEventListener("submit", function (e) {
+          const inputUser = document.getElementById("exampleInputEmail1").value;
+          let data = { correo: inputUser };
+          fetchData("http://localhost:4000/login", data).then((resolve) =>
+            setUser(resolve)
+          );
+
+          e.preventDefault();
+        });
+    },
+    [setUser]
+  );
+
   React.useEffect(() => {
-    // console.log("userContext (LOGIN): ", user);
     if (user) {
-      if (user.correo === "maikol@correo.com") {
-        navigate("/Inicio");
-      }
+      // navigate("/Inicio");
+      const nombre = user.correo;
+      const apellido = user.apellido;
+      console.table({ nombre, apellido });
     } else {
       console.log("Ingresa un usuario valido");
     }
   }, [user, navigate]);
 
-  // se busca que el perfil exista en el archivo json
-  function validarSesion() {
-    const correo = document.getElementById("exampleInputEmail1").value;
-
-    var data = { correo: correo };
-
-    var url = "http://localhost:4000/login";
-    fetch(url, {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((res) => res.json())
-      .catch((error) => console.error("Error:", error))
-      .then((response) => setUser(response));
-  }
-
   return (
-    <div>
-      <div
-        style={{ display: "flex", justifyContent: "center", marginTop: "1.5%" }}
-      >
-        <div style={{ width: "40%" }}>
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-              validarSesion();
-            }}
-          >
-            <div className="mb-3">
-              <label htmlFor="exampleInputEmail1" className="form-label">
-                Correo Electronico
-              </label>
-              <input
-                type="email"
-                name="correo"
-                className="form-control"
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
-              />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="exampleInputPassword1" className="form-label">
-                Contraseña
-              </label>
-              <input
-                type="password"
-                name="password"
-                className="form-control"
-                id="exampleInputPassword1"
-              />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="exampleInputEmail1" className="form-label">
-                Rol de Usuario
-              </label>
-              <select
-                className="form-select"
-                aria-label="Default select example"
-                id="rol"
-                name="rol"
-              >
-                <option value=""></option>
-                <option value="administrador">administrador</option>
-                <option value="mecanico">mecanico</option>
-                <option value="planta">planta</option>
-              </select>
-            </div>
-            <div className="mb-3 form-check">
-              <input
-                type="checkbox"
-                className="form-check-input"
-                id="exampleCheck1"
-              />
-              <label className="form-check-label" htmlFor="exampleCheck1">
-                Recordar sesion
-              </label>
-            </div>
-            <div style={{ textAlign: "center" }}>
-              <a className="dropdown-item" href="#!">
-                Olvidaste tu contraseña?
-              </a>
-            </div>
-            <div style={{ textAlign: "center" }}>
-              <button
-                type="submit"
-                className="btn btn-primary"
-                style={{ margin: "1%" }}
-              >
-                Ingresar
-              </button>
-              <button
-                type="reset"
-                className="btn btn-primary"
-                style={{ margin: "1%" }}
-              >
-                Borrar
-              </button>
-            </div>
-            <div className="dropdown-divider"></div>
-          </form>
+    <React.Fragment>
+      <form className="contentLogin" id="formulario">
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Correo Electronico
+          </label>
+          <input
+            type="email"
+            name="correo"
+            className="form-control"
+            id="exampleInputEmail1"
+          />
         </div>
-      </div>
-    </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">
+            Contraseña
+          </label>
+          <input
+            type="password"
+            name="password"
+            className="form-control"
+            id="exampleInputPassword1"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Rol de Usuario
+          </label>
+          <select className="form-select" id="rol" name="rol">
+            <option value=""></option>
+            <option value="administrador">administrador</option>
+            <option value="mecanico">mecanico</option>
+            <option value="planta">planta</option>
+          </select>
+        </div>
+        <div className="mb-3 form-check">
+          <input
+            type="checkbox"
+            className="form-check-input"
+            id="exampleCheck1"
+          />
+          <span className="form-check-label" htmlFor="exampleCheck1">
+            Recordar sesion
+          </span>
+        </div>
+        <div>
+          <a className="dropdown-item" href="#!">
+            Olvidaste tu contraseña?
+          </a>
+        </div>
+        <div>
+          <button type="submit" className="btn btn-primary">
+            Ingresar
+          </button>
+          <button type="reset" className="btn btn-primary">
+            Borrar
+          </button>
+        </div>
+        <div className="dropdown-divider"></div>
+      </form>
+    </React.Fragment>
   );
 }
--- Frontend/src/services/request.js
+++ Frontend/src/services/request.js
@@ -1,1 +1,17 @@
+export const fetchData = async function (url, user) {
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify(user),
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return new Error(error);
+  }
+};
+
 export const apiURL = process.env.REACT_APP_BACKEND_URL;
